6d43fd1adfb15d119ae9ab509177e7c3016beddc
Elijah Kamanga <elijahkamanga1@gmail.com>
2025-06-18 21:25:19 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// code your solution here
+// // code your solution here
+
+function saturdayFun(toDo="roller-skate"){
+    return `This Saturday, I want to ${toDo}!`
+}console.log(saturdayFun('go swimming'))
+
+
+const mondayWork=function(what="go to the office"){
+    return `This Monday, I will ${what}.`
+};
+console.log(mondayWork())
+console.log(mondayWork('go to the USA'))
+
+
+function wrapAdjective(ast="*"){
+    return function (adj="test"){
+        return `You are ${ast}${adj}${ast}!`;
+    };
+}
+const encourangingPromptFunction=wrapAdjective()
+console.log(encourangingPromptFunction("a smart person"))
+
